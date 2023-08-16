e0b3757222456d3f4d4741524197a74a60522f8a
Nathan <nathanrmsmith@gmail.com>
2023-08-16 15:54:58 -0400

    Add post method for form and random string gen

--- express_server.js
+++ express_server.js
@@ -3,6 +3,18 @@ const app = express();
 const PORT = 8000;
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+function generateRandomString() {
+  let result = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#*"
+  const charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength))
+  }
+
+  return result;
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -18,7 +30,16 @@ app.get("/urls", (req, res) => {
   res.render("urls_index.ejs", templateVars)
 })
 
-app.get('/urls/:id', (req, res) => {
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok")
+})
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new.ejs")
+})
+
+app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]}
   res.render("urls_show.ejs", templateVars)
 })
