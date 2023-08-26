ef61a32141ff407c754cba4957649a2291cffab8
Nathan <nathanrmsmith@gmail.com>
2023-08-26 19:14:41 -0400

    Remove @ from generateRandomString function as it caused bugs

--- helpers.js
+++ helpers.js
@@ -4,7 +4,7 @@
  */
 const generateRandomString = function() {
   let result = "";
-  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#*";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789#*";
   const charactersLength = characters.length;
   for (let i = 0; i < 6; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
